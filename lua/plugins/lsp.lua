2ec5c6c0d7ae4da8b476639d688484621febfeee
MrHlebusheck <lukavetskyihlib@gmail.com>
2025-03-30 15:17:08 +0200

    mason

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,10 +1,9 @@
-local success, lspSettings = pcall(require, "lsp-settings")
-if not success then
-  lspSettings = require("lsp-settings-default")
-end
-
 return {
   'neovim/nvim-lspconfig',
+  dependencies = {
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+  },
   config = function()
     local lspconfig = require('lspconfig')
     local capabilities = require('cmp_nvim_lsp').default_capabilities()
@@ -14,147 +13,70 @@ return {
       lineFoldingOnly = true
     }
 
-    if lspSettings.jdtls then
-      -- jdtls
-      lspconfig.jdtls.setup({
-        capabilities = capabilities,
-      })
-    end
+    require("mason").setup({})
+    require("mason-lspconfig").setup_handlers({
+      function(server)
+        local opts = {
+          capabilities = capabilities
+        }
 
+        if server == "lua_ls" then
+          opts = {
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                runtime = {
+                  version = "LuaJIT",
+                },
+                diagnostics = {
+                  globals = { "vim" },
+                },
+                workspace = {
+                  library = vim.api.nvim_get_runtime_file("", true),
+                },
+                telemetry = {
+                  enable = false,
+                },
+              },
+            }
+          }
+        end
 
-    if lspSettings.pylsp then
-      -- python-lsp-server python-rope python-pyflakes python-mccabe python-pycodestyle python-pydocstyle autopep8 flake8 python-pylint  python-lsp-isort
-      require 'lspconfig'.pylsp.setup {
-        settings = {
-          pylsp = {
-            plugins = {
-              isort = { enabled = true },
-              autopep8 = { enabled = true },
-              flake8 = { enabled = true },
-              -- pydocstyle = { enabled = true, ignore = {"D100", "D103"} },
-              pylint = { enabled = true, args = "--errors-only" },
-              rope_autoimport = {
-                enabled = true,
-                completions = { enabled = true },
-                code_actions = { enabled = true }
+        if server == "pylsp" then
+          opts = {
+            capabilities = capabilities,
+            settings = {
+              pylsp = {
+                plugins = {
+                  isort = { enabled = true },
+                  autopep8 = { enabled = true },
+                  flake8 = { enabled = true },
+                  -- pydocstyle = { enabled = true, ignore = {"D100", "D103"} },
+                  pylint = { enabled = true, args = "--errors-only" },
+                  rope_autoimport = {
+                    enabled = true,
+                    completions = { enabled = true },
+                    code_actions = { enabled = true }
+                  }
+                }
               }
             }
           }
-        }
-      }
-    end
+        end
 
-    if lspSettings.bashls then
-      -- bash-language-server
-      lspconfig.bashls.setup({
-        capabilities = capabilities
-      })
-    end
-
-    if lspSettings.lua_ls then
-      -- lua-language-server
-      lspconfig.lua_ls.setup({
-        capabilities = capabilities,
-        settings = {
-          Lua = {
-            runtime = {
-              version = "LuaJIT",
+        if server == "clangd" then
+          opts = {
+            capabilities = capabilities,
+            cmd = {
+              "clangd",
+              "--clang-tidy",
             },
-            diagnostics = {
-              globals = { "vim" },
-            },
-            workspace = {
-              library = vim.api.nvim_get_runtime_file("", true),
-            },
-            telemetry = {
-              enable = false,
-            },
-          },
-        }
-      })
-    end
+          }
+        end
 
-    if lspSettings.ts_ls then
-      -- npm install -g typescript typescript-language-server
-      lspconfig.ts_ls.setup({
-        capabilities = capabilities,
-      })
-    end
-
-    if lspSettings.html then
-      -- npm i -g vscode-langservers-extracted
-      lspconfig.html.setup({
-        capabilities = capabilities,
-      })
-    end
-
-    if lspSettings.cssls then
-      -- npm i -g vscode-langservers-extracted
-      lspconfig.cssls.setup({
-        capabilities = capabilities,
-      })
-    end
-
-    if lspSettings.eslint then
-      -- npm i -g vscode-langservers-extracted
-      lspconfig.eslint.setup({
-        capabilities = capabilities,
-      })
-    end
-
-    if lspSettings.jsonls then
-      -- npm i -g vscode-langservers-extracted
-      lspconfig.jsonls.setup({
-        capabilities = capabilities,
-      })
-    end
-
-    if lspSettings.emmet_ls then
-      -- npm install -g emmet-ls
-      lspconfig.emmet_ls.setup({
-        capabilities = capabilities
-      })
-    end
-
-    if lspSettings.clangd then
-      -- clang
-      lspconfig.clangd.setup({
-        capabilities = capabilities,
-        cmd = {
-          "clangd",
-          "--clang-tidy",
-        },
-      })
-    end
-
-    if lspSettings.dockerls then
-      -- npm install -g dockerfile-language-server-nodejs
-      lspconfig.dockerls.setup({
-        capabilities = capabilities,
-      })
-    end
-
-    if lspSettings.docker_compose_language_service then
-      -- npm install @microsoft/compose-language-service
-      lspconfig.docker_compose_language_service.setup({
-        capabilities = capabilities,
-      })
-    end
-
-    if lspSettings.autotools_ls then
-      -- autotools-language-server
-      lspconfig.autotools_ls.setup({
-        capabilities = capabilities
-      })
-    end
-
-    if lspSettings.cmake then
-      -- cmake-language-server
-      lspconfig.cmake.setup({
-        capabilities = capabilities
-      })
-    end
-
+        lspconfig[server].setup(opts)
+      end
+    })
 
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
       vim.lsp.diagnostic.on_publish_diagnostics, {
