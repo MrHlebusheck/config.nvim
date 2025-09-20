94d9e60181ffe7aec225296f83c2324142bdecfc
Hlib Lukavetskyi <lukavetskyihlib@gmail.com>
2025-09-20 19:32:51 +0200

    inline hints

--- lua/base/additional.lua
+++ lua/base/additional.lua
@@ -7,3 +7,11 @@ vim.fn.sign_define("DiagnosticSignInfo",
 vim.fn.sign_define("DiagnosticSignHint",
   { text = "󰌵", texthl = "DiagnosticSignHint" })
 
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
+    if client.server_capabilities.inlayHintProvider then
+      vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+    end
+  end
+})
