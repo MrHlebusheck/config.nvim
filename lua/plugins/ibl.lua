70eba8a49f37ef78cd29d839d79316db9fa694af
Hlib Lukavetskyi <lukavetskyihlib@gmail.com>
2025-10-10 09:50:34 +0200

    ibl.nvim

---
+++ lua/plugins/ibl.lua
@@ -0,0 +1,30 @@
+return {
+  "lukas-reineke/indent-blankline.nvim",
+  main = "ibl",
+  config = function()
+    require("ibl").setup({
+      exclude = {
+        filetypes = { "dashboard", "alpha", "starter", "neo-tree", "help", "lazy" },
+        buftypes = { "terminal", "nofile" },
+      },
+      indent = {
+        char = "│",
+      },
+      whitespace = {
+        highlight = { "Whitespace" },
+        remove_blankline_trail = false,
+      },
+      scope = {
+        enabled = true,
+        show_start = true,
+        show_end = false,
+        highlight = { "Function", "Label" },
+      },
+    })
+
+    vim.opt.list = true
+    vim.opt.listchars:append("space:·")
+    vim.opt.listchars:append("tab:→ ")
+    vim.opt.listchars:append("trail:•")
+  end,
+}
