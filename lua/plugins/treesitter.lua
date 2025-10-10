28c6f01be318f78227d51681661d976e87ab7617
Hlib Lukavetskyi <lukavetskyihlib@gmail.com>
2025-10-10 09:58:30 +0200

    treesitter-context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,6 +6,23 @@ return {
       'windwp/nvim-autopairs',
       event = "InsertEnter",
       opts = {}
+    },
+    {
+      'nvim-treesitter/nvim-treesitter-context',
+      event = "BufEnter",
+      opts = {
+        enable = true,
+        multiwindow = true,
+        max_lines = 0,
+        min_window_height = 0,
+        line_numbers = true,
+        multiline_threshold = 20,
+        trim_scope = 'outer',
+        mode = 'cursor',
+        separator = nil,
+        zindex = 20,
+        on_attach = nil,
+      }
     }
   },
   build = ":TSUpdate",
